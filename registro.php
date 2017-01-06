3a3ce9e46de46642a57d94028a3349e4d484d288
Erick Suarez <erick.suarez@outlook.com>
2017-01-06 10:19:56 -0400

    Add files via upload

---
+++ registro.php
@@ -0,0 +1,39 @@
+<?php
+$db_host="localhost";
+$db_user="cristoes";
+$db_password="5k7YLp3q9i";
+$db_name="cristoes_mipaginaweb";
+$db_table_name="formulario";
+   $db_connection = mysql_connect($db_host, $db_user, $db_password);
+ 
+if (!$db_connection) {
+	die('No se ha podido conectar a la base de datos');
+}
+$subs_name = utf8_decode($_POST['name']);
+$subs_email = utf8_decode($_POST['email']);
+$subs_tema = utf8_decode($_POST['tema']);
+$subs_mensaje = utf8_decode($_POST['mensaje']);
+ 
+$resultado=mysql_query("SELECT * FROM ".$db_table_name." WHERE email = '".$subs_email."'", $db_connection);
+ 
+
+ 
+
+ 
+
+	
+	$insert_value = 'INSERT INTO `' . $db_name . '`.`'.$db_table_name.'` (`name` , `email` , `tema`, `mensaje`) VALUES ("' . $subs_name . '", "' . $subs_email . '", "' . $subs_tema . '", "' . $subs_mensaje . '")';
+ 
+mysql_select_db($db_name, $db_connection);
+$retry_value = mysql_query($insert_value, $db_connection);
+ 
+if (!$retry_value) {
+   die('Error: ' . mysql_error());
+}
+	
+header('Location: index.html');
+
+ 
+mysql_close($db_connection);
+		
+?>
